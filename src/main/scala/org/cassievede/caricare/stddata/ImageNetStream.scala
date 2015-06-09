5f6d0dac000c29ac2b16e668eb809a484420fe14
Paul Wais <paulwais@gmail.com>
2015-06-08 19:08:10 -0700

    imagenet draft

--- src/main/scala/org/cassievede/caricare/stddata/ImageNetStream.scala
+++ src/main/scala/org/cassievede/caricare/stddata/ImageNetStream.scala
@@ -1,64 +1,128 @@
-///*
-// * Copyright 2015 Maintainers of CassieVede
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package org.cassievede.caricare.stddata
-//
-//import com.google.common.base.Preconditions.checkNotNull
-//import org.cassievede.caricare.datastream.Datastream
-//import scala.collection.mutable.HashMap
-//import java.io.File
-//import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
-//import java.io.FileInputStream
-//
-///**
-// * Generate a Datastream from a copy of the ImageNet tarball (~1.2TB)
-// *
-// * The ImageNet tarball has the following structure:
-// *   * Each file is a tarball named (wnid).tar.  This inner tarball
-// *       contains image files of the form (wnid)_(img number).JPEG
-// *   * There are 21841 total inner tarballs
-// */
-//class ImageNetStream(rootTar: File) extends Datastream {
-//
-//  private var mNext: HashMap[String, Any] = null
-//
-//  var rootPos :Long = 0
-//
-//  lazy val rootTarStream = {
-//    checkNotNull(rootTar)
-//    new TarArchiveInputStream(new FileInputStream(rootTar))
-//  }
-//
-//  def hasNext(): Boolean = {
-//
-//    /*
-//     * transient pos that will get reset to 0 after deser
-//     * non-transient `read` pos that will get serialized
-//     * hasNext() should skip up to read if not
-//     *
-//     *
-//     *
-//     * want a list of datasets that the stream induces, so main can
-//     *   create them if not exist; maybe make create require an 'already exist!'
-//     * want child entry -> record helper
-//     *
-//     */
-//
-//    return false
-//  }
-//
-//  def next() : HashMap[String, Any] = mNext
-//}
+/*
+ * Copyright 2015 Maintainers of CassieVede
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cassievede.caricare.stddata
+
+import com.google.common.base.Preconditions.checkNotNull
+import org.cassievede.caricare.datastream.Datastream
+import scala.collection.mutable.HashMap
+import java.io.File
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import java.io.FileInputStream
+import org.cassievede.caricare.ResumableTarStream
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+import org.apache.commons.io.FilenameUtils
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import java.nio.ByteBuffer
+
+/**
+ * Generate a Datastream from a copy of the ImageNet tarball (~1.2TB)
+ *
+ * The ImageNet tarball has the following structure:
+ *   * Each file is a tarball named (wnid).tar.  This inner tarball
+ *       contains image files of the form (wnid)_(img number).JPEG
+ *   * There are 21841 total inner tarballs
+ */
+class ImageNetStream(rootTar: File) extends Datastream {
+  val log: Log = LogFactory.getLog("ImageNetStream")
+
+  lazy val rootTarStream = new ResumableTarStream(rootTar)
+
+  var n: Long = 0
+
+  @transient var curIter: IterInnerTarEntries = null
+  var curIterStartOffset: Long = 0
+
+  @transient var curEntry: TarArchiveEntry = null
+
+  def hasNext(): Boolean = {
+    if (!rootTarStream.hasNext()) { return false }
+
+    if (curIter == null) {
+      curEntry = rootTarStream.next()
+      curIter =
+        new IterInnerTarEntries(
+              rootTarStream,
+              curEntry,
+              curIterStartOffset)
+    }
+
+    if (!curIter.hasNext()) {
+      curIter = null
+      curIterStartOffset = 0
+      return hasNext()
+    }
+
+    return curIter.hasNext()
+  }
+
+  def next() : HashMap[String, Any] = {
+    val r = curIter.next()
+    r("datasetName") = "ImageNet"
+    r("id") = n
+
+    n += 1
+
+    return r
+  }
+
+  ///
+  /// Utils
+  ///
+
+  class IterInnerTarEntries(
+       tarStream: ResumableTarStream,
+       curEntry: TarArchiveEntry,
+       startOffset: Long)
+    extends Datastream {
+
+      private var curInnerEntry: TarArchiveEntry = null
+
+      var wnid :String = ""
+
+      val innerTarStream = {
+        checkNotNull(curEntry)
+        log.info(f"Reading from entry ${curEntry.getName()}")
+        wnid = curEntry.getName().split(".tar")(0)
+        new TarArchiveInputStream(tarStream.inStream)
+      }
+
+      def hasNext() : Boolean = {
+        if (startOffset != 0) {
+          log.info(f"Resuming, skipping ${startOffset} bytes")
+          innerTarStream.skip(startOffset)
+        }
+        curInnerEntry = innerTarStream.getNextTarEntry()
+        return (curInnerEntry != null)
+      }
+
+      def next() : HashMap[String, Any] = {
+        val entryBytes =
+          ByteBuffer.allocate(curInnerEntry.getSize().asInstanceOf[Int])
+        tarStream.inStream.read(entryBytes.array())
+        log.debug(f"Read tar entry of ${curInnerEntry.getSize()} bytes")
+
+        val r = new HashMap[String, Any]
+        r("name") = curInnerEntry.getName()
+        r("classnames") = List(wnid)
+        r("data") = entryBytes
+
+        return r
+      }
+  }
+}
